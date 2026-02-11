5be9f88c66521d6a005f324a6c50376d6727894a
Ibrah kone <2335932@cegeplimoilou.ca>
2026-02-11 11:49:48 -0500

    - refactoring

--- src/test/java/org/climoilou/vetm/formatif1/CalculatriceTest.java
+++ src/test/java/org/climoilou/vetm/formatif1/CalculatriceTest.java
@@ -1,28 +1,40 @@
 package org.climoilou.vetm.formatif1;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatriceTest {
 
+    Calculatrice calculatrice;
+    final static String NUMBERS_EMPTY = "";
+    final static int NUMBER_ZERO = 0;
+    final static String ONE_NUMBER = "2";
+    final static int NUMBER_TWO = 2;
+
+    @BeforeEach
+     void setUp() throws Exception {
+        calculatrice = new Calculatrice();
+    }
 
     @Test
     public void givenEmptyString_whenAdd_thenReturnZero() {
-        String numbers = "";
+
         Calculatrice calculatrice = new Calculatrice();
 
-        int result = calculatrice.Add(numbers);
+        int result = calculatrice.Add(NUMBERS_EMPTY);
 
-        assertEquals(0,result);
+        assertEquals(NUMBER_ZERO, result);
 
     }
+
     @Test
-    public void givenTwoString_whenAdd_thenReturnTwo(){
-        String numbers = "2";
+    public void givenTwoString_whenAdd_thenReturnTwo() {
+
         Calculatrice calculatrice = new Calculatrice();
-        int result = calculatrice.Add(numbers);
-        assertEquals(2,result);
+        int result = calculatrice.Add(ONE_NUMBER);
+        assertEquals(NUMBER_TWO, result);
     }
 
 }
